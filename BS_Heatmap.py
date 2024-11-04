6e5d447f7687b8aaa171aeafae53261639c6ec28
Ryan Hernandez <153464107+ryanjh34@users.noreply.github.com>
2024-11-03 21:19:16 -0600

    Update BS_Heatmap.py

--- BS_Heatmap.py
+++ BS_Heatmap.py
@@ -3,13 +3,16 @@ import streamlit as st
 # Set the page configuration
 st.set_page_config(page_title="Split Layout Example", layout="wide")
 
+risk_free_rate = 4.25
+
 # Create a sidebar for inputs
 st.sidebar.title("Black-Scholes Pricing Model")
 stock_ticker = st.sidebar.text_input("Stock Ticker")
-input_number = st.sidebar.number_input("Enter a number:", min_value=0)
-
+strike_price = st.sidebar.number_input("Strike Price", min_value=0)
+expiration_date = st.sidebar.number_input("Expiration Date (YYYY-MM-DD)", min_value=0)
+st.sidebar.text(f"Current Risk-Free Rate: {risk_free_rate:.2%}")
 # Main area
 st.markdown('<div class="main-background">', unsafe_allow_html=True)
 st.title("Options Price - Interactive Heatmap")
 st.write("You entered: ", stock_ticker)
-st.write("Number entered: ", input_number)
+st.write("Number entered: ", strike_price)
