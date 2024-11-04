48d47c1dfd5c8f70c553f48ec4b3d13ec6da3d1e
Ryan Hernandez <153464107+ryanjh34@users.noreply.github.com>
2024-11-03 21:05:51 -0600

    Update BS_Heatmap.py

--- BS_Heatmap.py
+++ BS_Heatmap.py
@@ -25,6 +25,6 @@ input_number = st.sidebar.number_input("Enter a number:", min_value=0)
 # Main area
 st.markdown('<div class="main-background">', unsafe_allow_html=True)
 st.title("Display Section")
-st.write("You entered: ", input_text)
+st.write("You entered: ", stock_ticker)
 st.write("Number entered: ", input_number)
 st.markdown('</div>', unsafe_allow_html=True)
