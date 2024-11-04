574d4fcbad1a98400b3f3dff465aad0dd8111f18
Ryan Hernandez <153464107+ryanjh34@users.noreply.github.com>
2024-11-03 21:09:04 -0600

    Update BS_Heatmap.py

--- BS_Heatmap.py
+++ BS_Heatmap.py
@@ -3,8 +3,6 @@ import streamlit as st
 # Set the page configuration
 st.set_page_config(page_title="Split Layout Example", layout="wide")
 
-
-
 # Create a sidebar for inputs
 st.sidebar.title("Black-Scholes Pricing Model")
 stock_ticker = st.sidebar.text_input("Enter a stock ticker:")
@@ -12,7 +10,6 @@ input_number = st.sidebar.number_input("Enter a number:", min_value=0)
 
 # Main area
 st.markdown('<div class="main-background">', unsafe_allow_html=True)
-st.title("Display Section")
+st.title("Options Price - Interactive Heatmap")
 st.write("You entered: ", stock_ticker)
 st.write("Number entered: ", input_number)
-st.markdown('</div>', unsafe_allow_html=True)
